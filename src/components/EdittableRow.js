bc18f32c7c6b984d7d334956347f92c565fd2f06
Riste Boshev <60303629+riste238@users.noreply.github.com>
2023-04-05 08:23:22 +0200

    Add files via upload

---
+++ src/components/EdittableRow.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const EdittableRow = ({
+  editFormData,
+  handleEditFormChange
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a name..."
+          name="fullName"
+          value={editFormData.fullName}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter an address..."
+          name="address"
+          value={editFormData.address}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a phone number..."
+          name="phoneNumber"
+          value={editFormData.phoneNumber}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="email"
+          required="required"
+          placeholder="Enter an email..."
+          name="email"
+          value={editFormData.email}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <button type="submit">Save</button>
+      </td>
+    </tr>
+  );
+};
+
+export default EdittableRow;
